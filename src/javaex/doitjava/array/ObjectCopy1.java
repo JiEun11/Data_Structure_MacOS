51ae020e1fd2bcf3dac02cc56046cd2ab8cec032
Bella <shdudtnr3939@naver.com>
2021-12-25 12:26:17 +0900

    21.12.25. 객체배열 복사 System.arraycopy 연습

---
+++ src/javaex/doitjava/array/ObjectCopy1.java
@@ -0,0 +1,26 @@
+package javaex.doitjava.array;
+
+public class ObjectCopy1 {
+
+    public static void main(String[] args) {
+        Book[] bookArray1 = new Book[3];
+        Book[] bookArray2 = new Book[3];
+
+        for(Book book : bookArray1){
+            System.out.println(book);
+        }
+
+        bookArray1[0] = new Book("태백산맥","조정래");
+        bookArray1[1] = new Book("데미안", "헤르만 헤세");
+        bookArray1[2] = new Book("어떻게 살 것인가", "유시민");
+        System.arraycopy(bookArray1, 0, bookArray2, 0, 3);
+
+        for (Book book : bookArray2) {
+            book.showBookInfo();
+        }
+
+//        for(int i = 0; i < bookArray2.length ; i++){
+//            bookArray2[i].showBookInfo();
+//        }
+    }
+}
